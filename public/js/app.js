c399c14fb6a98bd15e65bb44846801f4b67cef0d
omerozturk <omerozturk90@outlook.com>
2019-10-26 19:18:20 +0300

    setup add for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javsacript is up and running');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('#txtSearch');
 
@@ -13,7 +11,7 @@ weatherForm.addEventListener('submit', (event) => {
     pMessage.innerHTML = 'Loading';
     pErrMessage.innerHTML = '';
 
-    fetch('http://localhost:3000/weather?address=' + location).then(response => {
+    fetch('/weather?address=' + location).then(response => {
     response.json().then(data => {
         if(data.error) {
             pMessage.innerHTML = '';
